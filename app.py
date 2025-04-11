a71cc84ac1af56b611c5950d7a4b818f27213c47
Riju Datta <dattarij@seas.upenn.edu>
2025-04-11 13:30:54 -0500

    Initial project setup

---
+++ app.py
@@ -0,0 +1,3 @@
+# This file contains the code for the interactive web app that allows a user to select a stock pair, see cointegration stats and spread/z-score plots,
+# simulate and visualize backtest results with adjustable thresholds and slippage, and use sliders for entry/exit z-test scores, holding period maxes, and 
+# slippage/txn cost. This web app is built using Streamlit.
